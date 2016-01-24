ec3aca67785240cbf59002c6158c64a915977803
Arvin Nguyen <arvinnguyen@ucla.edu>
2016-01-24 01:43:21 -0800

    Add company info

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 var sponsors = {
-  'Snapchat': {
+  'snapchat': {
     'name': 'Snapchat',
     'description': 'Snapchat is a fun messaging app for sharing moments. You can take a photo or a video, then add a caption or doodle or lense, and send it to a friend or add it to your story to share with the world/your followers. Friends can view individual snaps for up to 10 seconds, and then it disappears.',
     'positions': [{
@@ -15,7 +15,363 @@ var sponsors = {
                      'details': ['Part-Time', 'Spring 2016']
                   }],
     'logo': './img/logo-small.png'
-  }
+  },
+
+  'epoxy': {
+    'name': 'Epoxy, Inc',
+    'description': 'Epoxy builds exceptional software for short-form video creators and networks. We support the creator-fan relationship with critical technology allowing creators to share video and interact with fans wherever they spend time.',
+    'positions': [
+                 {
+                     'position': 'Full Stack Rails Engineer',
+                     'details': ['Full-Time']
+                 },
+                 {
+                     'position': 'Data Engineer',
+                     'details': ['Full-Time']
+                 },
+                 ],
+    'logo': './img/logo/epoxy.png'
+  },
+
+  'fluxchargers': {
+    'name': 'Flux Chargers',
+    'description': '',
+    'positions': [
+                 {
+                     'position': 'Full Stack Rails Engineer',
+                     'details': ['Full-Time']
+                 },
+                 {
+                     'position': 'Data Engineer',
+                     'details': ['Full-Time']
+                 },
+                 ],
+    'logo': './img/logo/',
+    'url' : 'http://epoxy.tv',
+  },
+
+  'fluxchargers': {
+    'name': 'Flux Chargers',
+    'description': 'Flux Chargers has one of the slimmest (6mm) and most beautiful portable chargers out there. With a built-in cord for Android and iPhone, the Flux Card™ is a truly universal phone charger. ',
+    'positions': [
+                 {
+                     'position': 'Marketing Intern',
+                     'details': ['All Year']
+                 },
+                 {
+                     'position': 'Software Engineer Intern',
+                     'details': ['All Year']
+                 },
+                 {
+                     'position': 'Sales Representatives',
+                     'details': ['All Year']
+                 },
+                 ],
+    'logo': './img/logo/fluxcharger.png',
+    'url' : 'http://fluxchargers.com',
+  },
+
+  'locent': {
+    'name': 'Locent',
+    'description': "We help companies build checkouts that run through text notifications. We're backed by Y-Combinator and run by UCLA alums.",
+    'positions': [
+                 {
+                     'position': 'Software engineering intern',
+                     'details': ['Part-time spring', 'Full-time summer']
+                 },
+                 ],
+    'logo': './img/logo/locent.png',
+    'url' : 'https://www.locent.com/',
+  },
+
+  'gustudios': {
+    'name': 'GuStudios',
+    'description': 'Encouraging the betterment of humanity and creativity through the creation of technology. ',
+    'positions': [
+                 {
+                     'position': 'Summer Software Engineer',
+                     'details': ['Full-Time', 'Part-time', 'Intern']
+                 },
+                 {
+                     'position': 'Summer Graphic Designer',
+                     'details': ['Full-Time', 'Part-time', 'Intern']
+                 },
+                 ],
+    'logo': './img/logo/gustudio.png',
+    'url' : 'http://www.gustudios.com/landing',
+  },
+
+  'tripscope': {
+    'name': 'TripScope',
+    'description': 'TripScope empowers travel agents to create, manage, and deliver customized itineraries. TripScope’s itinerary management website, mobile app, and advanced communication tools allow for interactive travel planning and world-class traveler support',
+    'positions': [
+                 {
+                     'position': 'Client relations representative',
+                     'details': []
+                 },
+                 {
+                     'position': 'Marketing relations representative',
+                     'details': []
+                 },
+                 {
+                     'position': 'Growth Hacker',
+                     'details': []
+                 },
+                 {
+                     'position': 'Interns',
+                     'details': []
+                 },
+                 {
+                     'position': 'Business development representatives',
+                     'details': []
+                 },
+                 ],
+    'logo': './img/logo/tripscope.png',
+    'url' : 'http://www.tripscopeapp.com',
+  },
+
+  'fluxla': {
+    'name': 'Flux.LA',
+    'description': 'Software Consultancy Services',
+    'positions': [
+                 {
+                     'position': 'Sales',
+                     'details': ['Year long']
+                 },
+                 {
+                     'position': 'Software engineering',
+                     'details': ['Intern', 'Spring/Summer']
+                 },
+                 {
+                     'position': 'Cllient Experience Intern',
+                     'details': ['Spring/Summer']
+                 },
+                 ],
+    'logo': './img/logo/fluxla.png',
+    'url' : 'http://flux.la',
+  },
+
+  'nimble': {
+    'name': 'Nimble',
+    'description': 'Nimble is a simply smarter sales and marketing solution. It allows you to track, engage, and nurture important contacts. It works everywhere you are: social sites, your inbox, etc. It delivers insights on any person or company you come across onlin',
+    'positions': [
+                 {
+                     'position': 'Social Media & Influencer Marketing Intern',
+                     'details': ['Part time', 'Summer']
+                 },
+                 {
+                     'position': 'Customer Data Analyst/Market Research Intern',
+                     'details': ['Part time', 'Summer']
+                 },
+                 {
+                     'position': 'Front End Engineering Intern',
+                     'details': ['Part time', 'Summer']
+                 },
+                 {
+                     'position': 'UI/UX Design Intern',
+                     'details': ['Part time', 'Summer']
+                 },
+                 {
+                     'position': 'Software Engineering Intern',
+                     'details': ['Part time', 'Summer']
+                 },
+                 ],
+    'logo': './img/logo/nimble.png',
+    'url' : 'http://www.nimble.com/',
+  },
+
+  'gridspace': {
+    'name': 'Gridspace',
+    'description': "Gridspace is a collaboration between SRI International, the lab behind Siri, and a multidisciplinary team of engineers. The company's software makes complex interactions easy to understand by making them searchable, accessible and organized. ",
+    'positions': [
+                 {
+                     'position': 'Software Engineering',
+                     'details': ['Intern']
+                 },
+                 {
+                     'position': 'Full-time generalist programmer',
+                     'details': []
+                 },
+                 {
+                     'position': 'NLP/ASR expert',
+                     'details': []
+                 },
+                 {
+                     'position': 'Front end developer',
+                     'details': []
+                 },
+                 ],
+    'logo': './img/logo/gridspace.png',
+    'url' : 'http://www.gridspace.com',
+  },
+
+  'eatthismuch': {
+    'name': 'Eat This Much',
+    'description': 'Eat This Much automatically creates meal plans that meet your diet goals. They can be personalized for your food preferences, budget, macros, and schedule. Then, once a week, we give you a grocery list with everything you need.',
+    'positions': [
+                 {
+                     'position': 'Software engineering',
+                     'details': ['Full-Time', 'Spring/Summer']
+                 },
+                 {
+                     'position': 'Marketing intern',
+                     'details': ['Spring/Summer']
+                 },
+                 {
+                     'position': 'Basic data entry',
+                     'details': ['part-time', 'Spring/Summer']
+                 },
+                 ],
+    'logo': './img/logo/eatthismuch.png',
+    'url' : 'https://www.eatthismuch.com/',
+  },
+
+  'videoamp': {
+    'name': 'VideoAmp',
+    'description': 'VideoAmp is a software platform built to enable the total video market. We do this by bridging the data divide between TV and Digital platforms. We enable content owners and advertisers to transact seamlessly across screens.',
+    'positions': [
+                 {
+                     'position': 'Sales Operations Assistant',
+                     'details': []
+                 },
+                 {
+                     'position': 'Digital Campaign Assistant',
+                     'details': []
+                 },
+                 {
+                     'position': 'Marketing/Business Development Assistant',
+                     'details': []
+                 },
+                 {
+                     'position': 'Data Researcher',
+                     'details': []
+                 },
+                 {
+                     'position': 'Operations Assistant',
+                     'details': []
+                 },
+                 ],
+    'logo': './img/logo/videoamp.png',
+    'url' : 'http://www.videoamp.com',
+  },
+
+  'moopon': {
+    'name': 'Moopon',
+    'description': "Moopon is a Customer-Sourced social media advertising platform for local merchants.  Users take pics and post on their social media via moopon's mobile app to get deals at participating merchants.",
+    'positions': [
+                 {
+                     'position': 'Developers',
+                     'details': ['iOS', 'Android', 'JAVA']
+                 },
+                 ],
+    'logo': './img/logo/moopon.jpg',
+    'url' : 'http://moopon.us',
+  },
+
+  'mkpartners': {
+    'name': 'MK Partners',
+    'description': 'We are small company that is agile like a startup but have the security of being in business since 2006. We value our employees above everything else.  We spend our days solving puzzles, building new things, and pushing the cloud to its limits.',
+    'positions': [
+                 {
+                     'position': 'Business Development Representative',
+                     'details': []
+                 },
+                 {
+                     'position': 'Technical Consultant',
+                     'details': []
+                 },
+                 {
+                     'position': 'Force.com Administrator',
+                     'details': []
+                 },
+                 {
+                     'position': 'Force.com Developer',
+                     'details': []
+                 },
+                 {
+                     'position': 'Software Engineer',
+                     'details': []
+                 },
+                 ],
+    'logo': './img/logo/mkpartners.png',
+    'url' : 'http://www.mkpartners.com',
+  },
+
+  'avant': {
+    'name': 'Avant',
+    'description': 'A hyper-growth fintech startup that offers access to personal loans with a process that is almost entirely online.',
+    'positions': [
+                 {
+                     'position': 'Software Engineer',
+                     'details': ['New Graduate']
+                 },
+                 {
+                     'position': 'Software Engineer',
+                     'details': ['Intern']
+                 },
+                 {
+                     'position': 'Business Data Analyst',
+                     'details': []
+                 },
+                 ],
+    'logo': './img/logo/avant.png',
+    'url' : 'http://www.avant.com',
+  },
+
+  'laserfiche': {
+    'name': 'Laserfiche',
+    'description': 'Laserfiche is a constant innovator in the field of enterprise content management software based out of Long Beach, CA.',
+    'positions': [
+                 {
+                     'position': 'Software engineering',
+                     'details': ['Full-Time']
+                 },
+                 {
+                     'position': 'Software engineering intern',
+                     'details': ['Summer']
+                 },
+                 {
+                     'position': 'Technical Marketing Engineer',
+                     'details': ['Full-Time']
+                 },
+                 {
+                     'position': 'Presales Consultant',
+                     'details': ['Full-Time']
+                 },
+                 {
+                     'position': 'Technical Writer for Software',
+                     'details': ['Full-Time']
+                 },
+                 ],
+    'logo': './img/logo/laserfiche.png',
+    'url' : 'http://www.laserfiche.com',
+  },
+
+  'gem': {
+    'name': 'Gem',
+    'description': 'Gem is a blockchain technology company in Venice, CA. Our team is dedicated to solving the world’s toughest problems with shared ledger technology. Gem’s platform unlocks the power of blockchains in finance, healthcare, insurance, supply chain and beyond',
+    'positions': [
+                 {
+                     'position': 'Junior software engineer (Front End)',
+                     'details': ['Full-Time']
+                 },
+                 {
+                     'position': 'Junior software engineer (Full stack)',
+                     'details': ['Full-Time']
+                 },
+                 {
+                     'position': 'QA/engineer in test',
+                     'details': ['Full-Time']
+                 },
+                 {
+                     'position': 'Software engineering internship',
+                     'details': ['full time', 'Spring/Summer']
+                 },
+                 ],
+    'logo': './img/logo/gem.png',
+    'url' : 'http://gem.co/',
+  },
+
 }
 
 var source = $("#tooltip-template").html();
@@ -27,10 +383,13 @@ var modal_template = Handlebars.compile(modal_source);
 $(document).ready(function() {
 
   // Tooltip hover
-  $('.logo').tooltipster({
-      delay: 150,
-      content: $('<span>' + tooltip_template(sponsors['Snapchat'])  + '</span>')
+  $('.logo').each(function() {
+    $(this).tooltipster({
+        delay: 150,
+        content: $('<span>' + tooltip_template(sponsors[this.id])  + '</span>')
+    });
   });
+
 
   // Modals
   $('.logo').click(function(e) {
